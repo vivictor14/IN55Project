4c42a0ff940957415d90b237ff4b3d8c73b6aa38
vivictor14 <victor.vogel@hotmail.fr>
2017-05-16 14:59:51 +0200

    Creation of the project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <QApplication>
+#include <QTextEdit>
+
+int main(int argv, char **args) {
+    QApplication app(argv, args);
+
+    QTextEdit textEdit;
+    textEdit.show();
+
+    return app.exec();
+}
