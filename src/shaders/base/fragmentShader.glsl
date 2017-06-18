ee650272aa1c1b68b40d89cf37ef7a7a56de35e3
Anthony <anthonygussy@orange.fr>
2017-06-18 22:27:22 +0200

    Test avec une atténuation

--- src/shaders/base/fragmentShader.glsl
+++ src/shaders/base/fragmentShader.glsl
@@ -1,34 +1,33 @@
 #version 330
 
-in vec3 TexCoords;
-
-out vec4 fColor;
-
 in vec4 amb;
 in vec4 diff;
 in vec4 spec;
 in vec3 Position;
 in vec3 ColorT;
+in float atte;
 
-in vec3 n;
+in vec3 Normale;
 
 uniform samplerCube skybox;
 uniform vec3 cameraPos;
 
+out vec4 fColor;
+
+
 void main()
-{
-vec3 ambient  = vec3(amb)  * vec3(ColorT);
-vec3 diffuse  = vec3(diff) * vec3(ColorT);
-vec3 specular = vec3(spec) * vec3(ColorT);
-float ratio = 1.00 / 2.42;
+    {
+    vec3 ambient  = vec3(amb)  * vec3(ColorT); //couleur ambiante
+    vec3 diffuse  = vec3(diff) * vec3(ColorT); //couleur diffuse
+    vec3 specular = vec3(spec) * vec3(ColorT); // couleur spéculaire
+    float ratio = 1.00 / 2.42; // ration passage de l'air au diamant
 
     vec3 I = normalize(Position - cameraPos);
 
-    vec3 Refraction = refract(I, n, ratio);
-    vec3 Reflection = reflect(I, n);
+    vec3 Refraction = refract(I, Normale, ratio);
+    vec3 Reflection = reflect(I, Normale);
 
-    fColor = vec4(ambient + diffuse + specular + texture(skybox, Reflection).rgb + texture(skybox, Refraction).rgb, 1.0);
+    fColor = vec4(ambient + diffuse*atte + specular*atte + texture(skybox, Reflection).rgb*atte+ texture(skybox, Refraction).rgb*atte, 1.0);
 
-       // fColor =  vec4(color,1); //* vec4(texture(skybox, Refract).rgb, 1.0) *  vec4(texture(skybox, Reflect).rgb, 1.0);
 }
 
--- src/shaders/base/vertexShader.glsl
+++ src/shaders/base/vertexShader.glsl
@@ -12,39 +12,10 @@ layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in vec3 normal;
 
-out vec3 TexCoords;
-
 uniform mat4 modelToWorld;
 uniform mat4 worldToCamera;
 uniform mat4 cameraToView;
-uniform ligth lumiere; // M
-
-//
-//void main()
-//{
-//
-//    vec3 LightPosition_worldspace = vec3(1.0f, 1.0f, 0.0f);
-//    // Output position of the vertex, in clip space : MVP * position
-//    gl_Position =  cameraToView * worldToCamera * modelToWorld * vec4(position,1);
-//
-//    // Position of the vertex, in worldspace : M * position
-//    vec3 Position_worldspace = (modelToWorld * vec4(position,1)).xyz;
-//
-//    // Vector that goes from the vertex to the camera, in camera space.
-//    // In camera space, the camera is at the origin (0,0,0).
-//    vec3 vertexPosition_cameraspace = ( worldToCamera * modelToWorld * vec4(position,1)).xyz;
-//    vec3 EyeDirection_cameraspace = vec3(0,0,0) - vertexPosition_cameraspace;
-//
-//    // Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
-//    vec3 LightPosition_cameraspace = ( worldToCamera * vec4(LightPosition_worldspace,1)).xyz;
-//    LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
-//
-//    // Normal of the the vertex, in camera space
-//    vec3 Normal_cameraspace = ( worldToCamera * modelToWorld * vec4(6.0f, 6.0f, 6.0f,0)).xyz; // Only correct if ModelMatrix does not scale
-//
-//     vColor = vec4(color, 0.3);
-//
-//}
+uniform ligth lumiere;
 
 out vec4 amb;
 out vec4 diff;
@@ -52,8 +23,9 @@ out vec4 spec;
 out vec3 ColorT;
 
 out vec3 Position;
-out vec3 n;
+out vec3 Normale;
 
+out float atte;
 
 
 void DirectionalLight(in vec3 V, in vec3 normal,inout vec4 ambient,inout vec4 diffuse, inout vec4 specular);
@@ -63,20 +35,18 @@ void main()
 {
 
     mat3 glNormalMatrix = transpose(inverse(mat3(modelToWorld)));
-    vec3 sp = vec3( worldToCamera * modelToWorld * vec4(position,1) );
+    vec3 sp = vec3( modelToWorld * vec4(position,1) );
     vec3 V = -normalize( sp );
     vec3 unit_normal = normalize( glNormalMatrix*normal.xyz );
     amb = vec4(0.0);  diff = vec4(0.0);  spec = vec4(0.0);
 
-//    DirectionalLight(V, unit_normal, amb, diff, spec);
+  //  DirectionalLight(V, unit_normal, amb, diff, spec);
     PointLight(V, sp, unit_normal, amb, diff, spec);
-
-    TexCoords = position;
 
     gl_Position =  cameraToView * worldToCamera * modelToWorld * vec4(position,1);
 
     ColorT = color;
-    n = unit_normal;
+    Normale = unit_normal;
     Position = vec3(modelToWorld * vec4(position,1));
 }
 
@@ -95,7 +65,7 @@ void DirectionalLight(in vec3 V, in vec3 normal,inout vec4 ambient,inout vec4 di
     else
     {
         nDotH = max(0.0, dot(normal, halfway_vector));
-        pf = pow(nDotH, 4*0.5);
+        pf = pow(nDotH, 4*0.6);
     }
     ambient += lumiere.ambiant;
     diffuse += lumiere.diffuse * nDotLi;
@@ -104,30 +74,31 @@ void DirectionalLight(in vec3 V, in vec3 normal,inout vec4 ambient,inout vec4 di
 
 void PointLight(in vec3 V, in vec3 sp, in vec3 normal, inout vec4 ambient, inout vec4 diffuse, inout vec4 specular )
 {
-    float nDotLi; // normal . light direction
-    float nDotH; // normal . light half vector
+    float nDotLi; // produit scalaire entre normal et direction de la lumière
+    float nDotH; // produit scalaire entre normal et le halfway_vector
     float pf; // power factor
-    float attenuation; // computed attenuation factor
-    float d;  // distance from surface to light source
-    vec3 L;  // direction from surface to light position
+    float attenuation;
+    float d;  // la distance entre la surface et la position de la lumière
+    vec3 L;  // la direction entre la surface et la position de la lumière
     vec3 halfway_vector;  // direction of maximum highlights
 
     L = lumiere.pos.xyz - sp;
-    d = length( L );
+    d = max(0.0,length( L ));
     L = normalize( L );
-    attenuation = 1.0 / (0.4 +
-    0.6 * d +
-    0.1 * d * d);
     halfway_vector = normalize(L + V);
     nDotLi = max(0.0, dot(normal, L));
     nDotH = max(0.0, dot(normal, halfway_vector));
 
+    attenuation = 1.0 / (0.4 + 0.01 *d*d);
+
     if (nDotLi == 0.0)
         pf = 0.0;
     else
-        pf = pow(nDotH, 42);
+        pf = pow(nDotH, 0.6);
 
-    ambient += lumiere.ambiant;
-    diffuse += lumiere.diffuse * nDotLi;
+// valeur trouver pour un materiau de type émeraude
+    ambient += lumiere.ambiant ;
+    diffuse += lumiere.diffuse * nDotLi ;
     specular += lumiere.specular * pf;
+    atte = attenuation;
 }
